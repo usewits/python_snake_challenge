270d7a40b23200f97eedfa7dca08b4752436dba4
usewits <abewits@gmail.com>
2015-04-29 21:13:19 +0000

    added mazes.py and player0

---
+++ snake/mazes.py
@@ -0,0 +1,39 @@
+import random
+
+#Please note: all coordinates are stored as [x, y]
+#one can obtain the data at the corresponding coordinate in a maze using maze[y][x]
+def generate_maze(w, h, global_density = 0.4, local_density = 2):
+    level = [['.' for x in range(w)] for y in range(h)]
+    for i in range(int(global_density*w*h)):
+        while True:
+            x = random.randint(1,w)-1
+            y = random.randint(1,h)-1
+            if level[y][x] != '.':
+                continue
+            c = 0
+            for dx in range(-1,2):
+                for dy in range(-1,2):
+                    if level[(y+dy+h)%h][(x+dx+w)%w] != '.':
+                        c+=1
+            if c > local_density:
+                continue
+            level[y][x] = '#'
+            break
+    return level
+
+def generate_maze2(w, h):
+    maze = [[".#"[(x%2)*(y%2)] for x in range(w)] for y in range(h)]
+    return maze
+
+def get_empty_cell(maze, w, h, c='None'):
+    while True:
+        x = random.randint(1,w)-1
+        y = random.randint(1,h)-1
+        if maze[y][x] == '.':
+            if c != 'None':
+                maze[y][x]=c
+            return [x,y]
+
+def show_maze(maze):
+    for line in maze:
+        print("".join(line))
